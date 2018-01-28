ec44d0730a17003b55684ee0b9b0b9817571212b
Ash Rawstron <ash.rawstron@gmail.com>
2018-01-28 10:56:49 -0800

    Add DraconainBehaviour to wrap overriding common behavior callback methods.

---
+++ com/draconianmarshmallows/boilerplate/DraconianBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace com.draconianmarshmallows.boilerplate
+{
+    public class DraconianBehaviour : MonoBehaviour
+    {
+        protected virtual void Awake() { }
+        protected virtual void Start() { }
+        protected virtual void Update() { }
+    }
+}
