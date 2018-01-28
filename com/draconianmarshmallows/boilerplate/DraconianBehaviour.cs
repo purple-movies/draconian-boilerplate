9ac905be4b3b9c0bd30349665ffc3915005074ac
Ash Rawstron <ash.rawstron@gmail.com>
2018-01-28 12:24:15 -0800

    Add on-trigger-enter callback to be overridden.

--- com/draconianmarshmallows/boilerplate/DraconianBehaviour.cs
+++ com/draconianmarshmallows/boilerplate/DraconianBehaviour.cs
@@ -7,5 +7,6 @@ namespace com.draconianmarshmallows.boilerplate
         protected virtual void Awake() { }
         protected virtual void Start() { }
         protected virtual void Update() { }
+        protected virtual void OnTriggerEnter(Collider other) { }
     }
 }
