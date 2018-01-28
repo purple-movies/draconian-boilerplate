22b8fde027dbf3b55e8c807d53e44a7565fc05d7
Ash Rawstron <ash.rawstron@gmail.com>
2018-01-28 09:47:58 -0800

    Add methods for resetting levels.

--- com/draconianmarshmallows/boilerplate/controllers/BaseLevelController.cs
+++ com/draconianmarshmallows/boilerplate/controllers/BaseLevelController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,25 @@ namespace com.draconianmarshmallows.boilerplate.controllers
 {
     public abstract class BaseLevelController : MonoBehaviour
     {
+        protected bool resetting;
+
         protected virtual void Start()
         {
             BaseMasterController.instance.onLevelStarted(this);
+            reset();
+            startLevel();
         }
+
+        protected virtual void reset()
+        {
+            resetting = true;
+        }
+
+        protected void endResetting()
+        {
+            resetting = false;
+        }
+
+        protected virtual void startLevel() { }
     }
 }
