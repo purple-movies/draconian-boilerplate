1a1b418677733567c0f98bedbe13f9181d499562
Ash Rawstron <ash.rawstron@gmail.com>
2018-02-18 12:30:25 -0800

    Add UI ButtonController.

---
+++ com/draconianmarshmallows/boilerplate/ui/ButtonController.cs
@@ -0,0 +1,20 @@
+﻿using com.draconianmarshmallows.boilerplate;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonController : DraconianBehaviour
+{
+    [SerializeField] private Button button;
+    [SerializeField] private EventTrigger eventTrigger;
+
+
+    protected override void Start()
+    {
+        base.Start();
+
+
+    }
+}
