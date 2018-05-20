cc7b9e3183150b3f5b1fa64af54936a2d6fdec4e
Ash Rawstron <ash.rawstron@gmail.com>
2018-05-20 13:00:12 -0600

    Add 2D trigger enter callback.

--- com/draconianmarshmallows/boilerplate/DraconianBehaviour.cs
+++ com/draconianmarshmallows/boilerplate/DraconianBehaviour.cs
@@ -7,5 +7,6 @@ namespace com.draconianmarshmallows.boilerplate
         protected virtual void Awake() { }
         protected virtual void Start() { }
         protected virtual void OnTriggerEnter(Collider other) { }
+        protected virtual void OnTriggerEnter2D(Collider2D other) { }
     }
 }
