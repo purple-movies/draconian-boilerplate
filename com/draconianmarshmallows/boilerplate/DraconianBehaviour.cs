faaf207a67424c62f400cb3fada688aca02b58a3
PowerAsh <ash.rawstron@gmail.com>
2019-02-03 18:40:34 -0700

    Add on-collision 2D callback.

--- com/draconianmarshmallows/boilerplate/DraconianBehaviour.cs
+++ com/draconianmarshmallows/boilerplate/DraconianBehaviour.cs
@@ -6,6 +6,8 @@ namespace com.draconianmarshmallows.boilerplate
     {
         protected virtual void Awake() { }
         protected virtual void Start() { }
+        protected virtual void OnDestroy() { }
+        protected virtual void OnCollisionEnter2D(Collision2D collision) { }
         protected virtual void OnTriggerEnter(Collider other) { }
         protected virtual void OnTriggerEnter2D(Collider2D other) { }
     }
--- com/draconianmarshmallows/boilerplate/controllers/BasePlayerController.cs
+++ com/draconianmarshmallows/boilerplate/controllers/BasePlayerController.cs
@@ -5,6 +5,21 @@ using System.Text;
 
 namespace com.draconianmarshmallows.boilerplate.controllers
 {
-    // NOTE(ASH): Adding this as a placeholder - could probably be a generic-object. 
-    public class BasePlayerController : DraconianBehaviour { }
+    public class BasePlayerController : DraconianBehaviour
+    {
+        public static BasePlayerController Instance{ get; private set; }
+
+        protected override void Start()
+        {
+            base.Start();
+            if (Instance) Destroy(gameObject);
+            Instance = this;
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            Instance = null;
+        }
+    }
 }
