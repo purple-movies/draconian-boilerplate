74e2ff2aa787570657d2a13d1c3b82f3006f0849
PowerAsh <ash.rawstron@gmail.com>
2019-02-17 18:36:38 -0700

    Add accessible on-trigger-enter-2D callback and parent-delegate.

---
+++ com/draconianmarshmallows/boilerplate/DelegateToParent.cs
@@ -0,0 +1,14 @@
+﻿using com.draconianmarshmallows.boilerplate;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateToParent : MonoBehaviour
+{
+    public DraconianBehaviour parentDelegate;
+
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (parentDelegate != null) parentDelegate.OnTriggerEnter2D(collision);
+    }
+}
--- com/draconianmarshmallows/boilerplate/DraconianBehaviour.cs
+++ com/draconianmarshmallows/boilerplate/DraconianBehaviour.cs
@@ -4,11 +4,12 @@ namespace com.draconianmarshmallows.boilerplate
 {
     public class DraconianBehaviour : MonoBehaviour
     {
+        public virtual void OnTriggerEnter2D(Collider2D other) { }
+
         protected virtual void Awake() { }
         protected virtual void Start() { }
         protected virtual void OnDestroy() { }
         protected virtual void OnCollisionEnter2D(Collision2D collision) { }
         protected virtual void OnTriggerEnter(Collider other) { }
-        protected virtual void OnTriggerEnter2D(Collider2D other) { }
     }
 }
